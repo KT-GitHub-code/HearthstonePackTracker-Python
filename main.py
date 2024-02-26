1e745cce72aacdd8bcf332614bf467581969ff39
KT <krakovszkyt@gmail.com>
2024-02-26 15:29:18 +0100

    Add plotting

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+# import sys
 import json
 
 
@@ -5,7 +7,34 @@ def main_function():
     file_path = "../HearthstonePackTracker-Backend/FullPackHistory.json"
     json_data = read_json_file(file_path)
 
-    runs = get_pack_histories_from_json_data(json_data)
+    runs_dict = get_pack_histories_from_json_data(json_data)
+
+    # pack_type = sys.argv[1]
+    pack_type = "Classic"
+
+    plot_pack_history(pack_type, runs_dict)
+
+
+def plot_pack_history(pack_type, runs_dict):
+    runs = runs_dict[pack_type]
+
+    plt.figure(figsize=(8, 6), dpi=100)
+
+    plt.scatter(range(1, len(runs) + 1), runs)
+
+    plt.xticks(range(1, len(runs) + 1))
+    plt.yticks(range(0, 41, 5))
+
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+    for i, txt in enumerate(runs):
+        plt.annotate(txt, (i + 1, runs[i]), textcoords="offset points", xytext=(0, 5), ha='center')
+
+    plt.xlabel('Runs')
+    plt.ylabel('Opened packs')
+    plt.title(pack_type)
+
+    plt.show()
 
 
 def get_pack_histories_from_json_data(json_data):
